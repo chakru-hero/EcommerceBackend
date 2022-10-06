58f3669731cc192dccc556ed6b29e2fb8f447829
chakru-hero <chakradhar.dwivedi2000@gmail.com>
2022-10-07 02:45:30 +0530

    CartPage added

--- ChakruPortfolio/src/main/java/com/portfolio/chakru/controllers/CartPageController.java
+++ ChakruPortfolio/src/main/java/com/portfolio/chakru/controllers/CartPageController.java
@@ -8,17 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.portfolio.chakru.models.CartModel;
-import com.portfolio.chakru.models.UserModel;
 import com.portfolio.chakru.repo.CartRepo;
-import com.portfolio.chakru.service.CartService;
 import com.portfolio.chakru.service.UserService;
 
 @Controller
 @RequestMapping("/cart")
 public class CartPageController {
 
-	@Autowired
-	private CartService cartService;
+//	@Autowired
+//	private CartService cartService;
 
 	@Autowired
 	private CartRepo cartrepo;
@@ -29,8 +27,7 @@ public class CartPageController {
 	@GetMapping("/{userid}")
 	public String getCart(@PathVariable("userid") Long userid, Model model) {
 
-		UserModel userModel = userService.findUserModelById(userid);
-		CartModel cartModel = cartrepo.findCartModelByUser(userModel);
+		CartModel cartModel = cartrepo.findCartModelByUser(userService.findUserModelById(userid));
 		model.addAttribute("cart" ,cartModel);
 		return "Cartpage";
 	}
--- ChakruPortfolio/src/main/java/com/portfolio/chakru/controllers/ProductPageController.java
+++ ChakruPortfolio/src/main/java/com/portfolio/chakru/controllers/ProductPageController.java
@@ -16,6 +16,7 @@ import com.portfolio.chakru.service.CartService;
 import com.portfolio.chakru.service.ProductService;
 import com.portfolio.chakru.service.UserService;
 
+@SuppressWarnings("unused")
 @Controller
 @RequestMapping("/p")
 public class ProductPageController {
